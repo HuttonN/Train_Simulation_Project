1175e72d3d1f32d9e7fe71884ac79309ff673f56
justneil11 <justneil11@gmail.com>
2025-07-05 15:25:21 +0100

    edited track_loader to be able to deal with new track class

--- controller/track_loader.py
+++ controller/track_loader.py
@@ -4,6 +4,7 @@ from core.track.straight import StraightTrack
 from core.track.curve import CurvedTrack
 from core.track.junction import JunctionTrack
 from core.track.station import StationTrack
+from core.track.double_curve_junction import DoubleCurveJunctionTrack
 
 def load_track_layout(json_path, grid):
     with open(json_path, 'r') as file:
@@ -21,6 +22,8 @@ def load_track_layout(json_path, grid):
             track_object = JunctionTrack(grid, *track['start'], *track['straight_end'], *track['curve_control'], *track['curve_end'], tid, ttype)
         elif ttype == 'station':
             track_object = StationTrack(grid, *track['start'], *track['end'], track['name'], tid, ttype)
+        elif ttype == 'double_curve_junction':
+            track_object = DoubleCurveJunctionTrack(grid, *track['start'], *track['right_curve_control'], *track['left_curve_control'], *track['right_curve_end'], *track['left_curve_end'], tid, ttype)
         else:
             raise ValueError(f"Unknown track type: {ttype}")
         track_object.connections = track["connections"]
